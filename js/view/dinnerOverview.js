8f98924403668aac05a20388e14d7061f387d671
IsabellaJohannesson <isabellajohannesson@gmail.com>
2018-02-21 14:48:58 +0100

    Update dinnerOverview.js

--- js/view/dinnerOverview.js
+++ js/view/dinnerOverview.js
@@ -4,26 +4,16 @@ var DinnerOverView = function (container,model) {
 
 	// Observer som uppdaterar vyn när numOfGuests uppdateras i model 
 	model.addObserver(this);
-	
-	
-	// Element att addera HTML I
-	this.title = container.find("#dinner-overview-mydinner");
-	this.dishes = container.find("#dinner-overview-bottom");
-
-	// Addering av element
-	this.title.html("My Dinner: " + model.getNumberOfGuests() + " people");
-	this.dishes.html("");
-	
-	this.menu = model.getFullMenu();
-	for(i in this.menu) {
-	var dish = this.menu[i];
-	this.dishes.append('<div class="dishlistbox"><div class="dinner-overview-image" style="background-image: url(images/' + dish['image'] + ')"><span class="dinner-overview-title">' + dish['name'] + '</span></div>'
-			 + '<span class="dinner-overview-price">' + model.getDishPrice(dish['id']) + ' SEK</span></div>');
-	}
 
 	// Uppdaterar vyn när numOfGuests eller addDish ändras
 	this.update = function(args){
 		if (args == "numOfGuests" || args == "addDish") {
+
+			// Element att addera HTML I
+			this.title = container.find("#dinner-overview-mydinner");
+			this.dishes = container.find("#dinner-overview-bottom");
+
+			// Addering av element i HTML
 			this.title.html("My Dinner: " + model.getNumberOfGuests() + " people");
 			this.dishes.html("");
 			
