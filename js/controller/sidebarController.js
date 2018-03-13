32d6ac5240b3ae8c01992f33031fff0161e40271
IsabellaJohannesson <isabellajohannesson@gmail.com>
2018-03-13 16:59:46 +0100

    Update sidebarController.js

--- js/controller/sidebarController.js
+++ js/controller/sidebarController.js
@@ -2,25 +2,26 @@
 var SidebarController = function (view,model) {
 	
 
-	$("#minusGuest").click(function() {
+	view.minusButton.click(function() {
 		console.log("minusGuest");
 		model.setNumberOfGuests(model.getNumberOfGuests() - 1);
 		console.log(model.getNumberOfGuests());
 	});
 
 
-	$("#plusGuest").click(function() {
+	view.plusButton.click(function() {
 		console.log("plusGuest");
 		model.setNumberOfGuests(model.getNumberOfGuests() + 1);
 		console.log(model.getNumberOfGuests());
 	});
 
 
-	view.confirmbutton.click(function() {
-		console.log("#sidebar-confirm-btn");
+	view.confirmButton.click(function() {
+		console.log("sidebar-confirm-btn");
 		showDinnerOverviewScreen();
 	});
 
 	
 
 }
+
