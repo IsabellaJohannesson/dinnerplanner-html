ff8e4c5dbf6451f3da9283fe66096c54aa7dbcab
IsabellaJohannesson <isabellajohannesson@gmail.com>
2018-03-13 17:00:10 +0100

    Update dishsearchController.js

--- js/controller/dishsearchController.js
+++ js/controller/dishsearchController.js
@@ -1,35 +1,34 @@
-//ExampleView Object constructor
 var DishSearchController = function (view,model) {
-	
-	// Vid klick av typ av rätt	
-	$("#select-type").change(function(){
-	var type = $(this).val();
-	console.log("typeselector" + type);
 
-	view.selectedDishType(type);
+	view.searchForm.on('submit', function(e){
+		e.preventDefault();
+	})
+
+
+	view.searchButton.click(function(){
+		var value = view.typeValue.val();
+		console.log("Value is:" ,value)
+		var search = view.searchField.val();
+		model.setFilter(search);
+		model.setType(value);
+		view.update("searchResults");
+
 	});
 
-	// Vid klick av sökknappen
-	$("#dish-search-search-btn").click(function(){
-		var type = $("#select-type").val();
-		var filter = $("#search-field").val();
-		console.log("searchterm: " + filter + " searchtype: " + type);
-		view.searchDish(type,filter); 
+
+	view.dishes.on('click', '.dishlistbox .dish-search-image', function() {
+			var dishId = $(this).attr('id');
+			model.getDish(dishId, function(data) {
+				model.setCurrentDish(data);
+				showDishDetailsViewScreen();
+				showSideBarViewScreen();
+			})
 	
+			
 	});
-
-
-	// Vid klick av bild för att komma till dishdetailsView.js
-	$(".dish-search-image").click(function() {
-		var id = $(this).find(".dish-id").text();
-		model.setSelectedDishId(id);
-		console.log("dish-search-image " + id);
-		$('#dish-search-view').hide();
-		$('#dish-details-view').show();
-		$('#pendingprice').html(model.getDishPrice(id));
-		$('#pendingrow').show();
-		});
-
-
 }
+
+
+
+
  
