566092050eab4f329ba059da7fbc2210c342b513
IsabellaJohannesson <isabellajohannesson@gmail.com>
2018-03-13 16:15:03 +0100

    Update dishdetailsview.js

--- js/view/dishdetailsview.js
+++ js/view/dishdetailsview.js
@@ -1,57 +1,51 @@
-//ExampleView Object constructor
-var DishDetailsView = function (container,model) {
-	
-	var dishId = model.getSelectedDishId();
+var DishDetailsView = function(container, model) {
 
-	// Vänster 
-	this.title = container.find(".dish-details-title")
-	this.image = container.find(".dish-details-image");
+	var title = container.find(".dish-details-title");
+    var image = container.find(".dish-details-image");
+    var description = container.find(".dish-details-summary");
 
-	// Höger
-	this.ingredientsListtitle = container.find("#ingredientsList-title")
-	this.ingredients = container.find("#ingredientsList-ingredients");
-	this.priceSum = container.find('#priceSum');
+    var dishPrice = container.find("#dishPrice");
+    var dishTime = container.find("#ingredientsList-title")
 
-	// Nedre
-	this.description = container.find(".dish-details-description");
-
-	// Uppdaterar när vi ändrar this.notifyObserver("numOfGuests"); i // Höger 
-	this.update = function(args) {
-		if (args == "numOfGuests") {
-			this.dish = model.getDish(model.getSelectedDishId());
-			this.ingredientsListtitle.html("Ingredients for "+ model.getNumberOfGuests() + " people.");
-			this.priceSum.html(model.getDishPrice(this.dish["id"]));
-			
-			var ingredient = this.dish['ingredients'];
-			this.ingredients.html("");
-
-			for (i in ingredient) {
-				this.ingredients.append("<tr><td>" + ingredient[i]['quantity']*model.getNumberOfGuests() + " " + ingredient[i]['unit'] + "</td><td>" + ingredient[i]['name'] + "</td><td>SEK</td><td>" + ingredient[i]['price']*model.getNumberOfGuests() + "</td></tr>");
-			};
-
-		} 
-
-	// Ger oss vyn vid tidigare tillstånd från föregående sida
-		else if(args == "selectedDishId") {
-			this.dish = model.getDish(model.getSelectedDishId());
-			this.ingredientsListtitle.html("Ingredients for "+ model.getNumberOfGuests() + " people.");
-			this.priceSum.html(model.getDishPrice(this.dish["id"]));
-			this.title.html(this.dish['name']);
-			this.image.html("<img class='dish' src=images/" + this.dish['image'] + ">");
-			this.description.html(this.dish['description']);
-
-			var ingredient = this.dish['ingredients'];
-			this.ingredients.html("");
-
-			for (i in ingredient) {
-				this.ingredients.append("<tr><td>" + ingredient[i]['quantity']*model.getNumberOfGuests() + " " + ingredient[i]['unit'] + "</td><td>" + ingredient[i]['name'] + "</td><td>SEK</td><td>" + ingredient[i]['price']*model.getNumberOfGuests() + "</td></tr>");
-			};
-		}
-	}
-
-this.update();
-model.addObserver(this);
-
+    this.addToMenu = container.find('#dish-details-confirm-btn');
+	this.goBack = container.find("#dish-details-back-btn");
 	
 
+    this.update = function(){
+        var ingredient ='';
+        var amount='';
+        var unit ='';
+        
+        if(model.getCurrentDish() != 0){
+
+            var dish = model.getCurrentDish();
+            var numOfGuests = model.getNumberOfGuests();
+            
+            for(var i=0; i < dish.extendedIngredients.length; i++){
+
+                ingredient += dish.extendedIngredients[i].name + '<br>';
+                amount += Math.round(dish.extendedIngredients[i].amount * numOfGuests) + '<br>';
+                unit += dish.extendedIngredients[i].unit + '<br>';
+
+                };
+
+                var imgHtml = '<img src="' + dish.image + '"/>';
+                image.html(imgHtml);
+
+
+                title.html(dish.title);
+                description.html(dish.instructions);
+
+                container.find('#td1').html(ingredient);
+                container.find('#td2').html(amount);
+                container.find('#td3').html(unit);
+                container.find('#numOfGuests').html(numOfGuests);
+
+                dishPrice.html(Math.round(model.getCurrentDish().pricePerServing * numOfGuests));
+                dishTime.html("Preparation time: " + dish.readyInMinutes + " min");
+        }
+        
+    };
+
+    model.addObserver(this);
 }
