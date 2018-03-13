77d90ac3c69f7d44f486ebb8021fa3b9faf1bd13
IsabellaJohannesson <isabellajohannesson@gmail.com>
2018-03-13 17:20:28 +0100

    Update dinnerprintoutController.js

--- js/controller/dinnerprintoutController.js
+++ js/controller/dinnerprintoutController.js
@@ -1,11 +1,9 @@
 //ExampleView Object constructor
 var DinnerPrintOutController = function (view,model) {
 
-		$("#dinner-printout-back-btn").click(function(){
+		view.goBack.click(function(){
 		console.log("DP-confirm-btn");
-		$('#dinner-printout-view').hide();
-		$('#dish-search-view').show();
-		$('#sidebar-view').show();
-		//view.update("numberOfGuests");
+		showDishSearchViewScreen();
+		showSideBarViewScreen();
 	});
 }
